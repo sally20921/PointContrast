b3d43bbeffa50e0ea952ec013757d49df1ac16cf
sally20921 <sally20921@snu.ac.kr>
2021-02-28 12:08:00 -0500

    adding data

---
+++ pretrain/data_preprocess/scannet_pair/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export TARGET_DIR=/home/data/scannet
+export SCANNET_DIR=/home/data/scannet
+export FRAME_SKIP=25
+export JOBS=50
+
+reader() {
+    filename=$1
+
+    scene=$(basename -- "$filename")
+    scene="${scene%.*}"
+    echo "Find sens data: $filename $scene"
+    python -u reader.py --filename $filename --output_path $TARGET_DIR/$scene --frame_skip $FRAME_SKIP --export_depth_images --export_poses --export_intrinsics
+    echo "Extract point-cloud data"
+    python -u point_cloud_extractor.py --input_path $TARGET_DIR/$scene --output_path $TARGET_DIR/$scene/pcd --save_npz
+    echo "Compute partial scan overlapping"
+    python -u compute_full_overlapping.py --input_path $TARGET_DIR/$scene/pcd
+}
+export -f reader
+
+
+parallel -j $JOBS --linebuffer time reader ::: `find  $SCANNET_DIR/scans/scene*/*.sens`
